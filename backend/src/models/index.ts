14c5115c7d8cbeb4fca8a75e8916ec842e26ba0a
aryan083 <aryanmahida2@gmail.com>
2026-01-29 00:21:08 +0530

    Fix tests: remove broken Leaderboard re-export

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -9,7 +9,6 @@ export { QuizAttempt } from './QuizAttempt';
 export { QuizAttemptAnswer } from './QuizAttemptAnswer';
 export { Match } from './Match';
 export { MatchPlayer } from './MatchPlayer';
-export { Leaderboard } from './Leaderboard';
 
 // Export all enums from shared location
 export { 
